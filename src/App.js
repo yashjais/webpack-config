723bfcb9ac5b7bbc336ecd81beb3d7c429e33b97
Yash Jaiswal <jais.yash01@gmail.com>
2025-12-17 01:42:50 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App () {
+  return (
+    <div>
+      <h1>Hello from React + Webpack!</h1>
+      <p>Bundle size matters!</p>
+    </div>
+  )
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.[contenthash].js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    hot: true,
+  },
+  mode: 'development',
+};
